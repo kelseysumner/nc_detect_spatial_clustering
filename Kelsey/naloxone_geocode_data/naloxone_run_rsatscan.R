84e6392037821a65f357479a022489ebb1d16773
Kelsey Sumner <sumner.kelsey@gmail.com>
2019-06-06 08:37:02 -0400

    Create naloxone_run_rsatscan.R

---
+++ Kelsey/naloxone_geocode_data/naloxone_run_rsatscan.R
@@ -0,0 +1,19 @@
+# ----------------------------------------------- #
+#          NC DETECT Spatial Project              #
+#           Naloxone Geocoded Data                #
+#      Try running rsatscan on the data set       #
+#              June 6, 2019                       #
+#                K. Sumner                        #
+# ----------------------------------------------- #
+
+# what this is doing: 
+# reading in the naloxone shapefile (by day) and running rsatscan on it
+
+
+#### ------- load the libraries ---------- ####
+
+# load in the libraries of interest
+library(rsatscan)
+
+
+#### ------- read in the data sets ---------- ####
