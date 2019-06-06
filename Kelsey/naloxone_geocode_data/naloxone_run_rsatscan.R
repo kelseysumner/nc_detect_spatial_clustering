e2a05c6738a3c0fc42e476044195a6655fc5d200
Kelsey Sumner <sumner.kelsey@gmail.com>
2019-06-06 11:35:43 -0400

    Update naloxone_run_rsatscan.R

--- Kelsey/naloxone_geocode_data/naloxone_run_rsatscan.R
+++ Kelsey/naloxone_geocode_data/naloxone_run_rsatscan.R
@@ -14,6 +14,24 @@
 
 # load in the libraries of interest
 library(rsatscan)
+library(tidyverse)
 
 
 #### ------- read in the data sets ---------- ####
+
+# set working directory
+setwd("C://Users//kelseyms//OneDrive - University of North Carolina at Chapel Hill/nc_detect_one_drive/Naloxone Geocoded Data")
+
+# read in the cleaned naloxone data with the lat and long centroid pulled out
+nalox_data = read_csv("./clean_nalox_data_latlong.csv")
+
+
+
+#### ------- try running rsatscan on the data set --------- ####
+
+
+
+
+
+
+
