ab492f3c641274f32fe4270f3d0e8ef0e6a8e728
Kelsey Sumner <sumner.kelsey@gmail.com>
2019-08-01 15:48:40 -0400

    updated influenza maps and started to make influenza plots

--- Kelsey/influenza_data/ncdetect_influenza_data_make_maps.R
+++ Kelsey/influenza_data/ncdetect_influenza_data_make_maps.R
@@ -7,8 +7,7 @@
 # ----------------------------------------- #
 
 # what this is doing: 
-# reading in the naloxone geocoded data set, merging with census data at the tract level
-
+# reading in the zip code influenza data and making maps of clusters identified aggregating data by week
 
 #### ------- load the libraries ---------- ####
 
@@ -22,13 +21,13 @@ library(rgdal)
 
 if (str_detect(tolower(Sys.info()["user"]), "kelsey")) {
   user = "kelsey"
-  wd_cconly = "C:\\Users\\kelseyms\\OneDrive - University of North Carolina at Chapel Hill\\nc_detect_one_drive\\Influenza Data\\cc_only\\180_days_WEEKLY\\shapefiles"
-  wd_cctriage = "C:\\Users\\kelseyms\\OneDrive - University of North Carolina at Chapel Hill\\nc_detect_one_drive\\Influenza Data\\ccandtriagenotes\\180_days_WEEKLY\\shapefiles"
+  wd_cconly = "C:\\Users\\kelseyms\\OneDrive - University of North Carolina at Chapel Hill\\nc_detect_one_drive\\Influenza Data\\cc_only\\180_days_WEEKLY\\weekly_shapefiles"
+  wd_cctriage = "C:\\Users\\kelseyms\\OneDrive - University of North Carolina at Chapel Hill\\nc_detect_one_drive\\Influenza Data\\ccandtriagenotes\\180_days_WEEKLY\\weekly_shapefiles"
   
 } else if (str_detect(tolower(Sys.info()["user"]), "joyce")) {
   user = "joyce"
-  wd_cconly = "C:\\Users\\joyceyan\\University of North Carolina at Chapel Hill\\Sumner, Kelsey Marie - nc_detect_one_drive\\Influenza Data\\cc_only\\180_days_WEEKLY\\shapefiles"
-  wd_cctriage = "C:\\Users\\joyceyan\\University of North Carolina at Chapel Hill\\Sumner, Kelsey Marie - nc_detect_one_drive\\Influenza Data\\ccandtriagenotes\\180_days_WEEKLY\\shapefiles"
+  wd_cconly = "C:\\Users\\joyceyan\\University of North Carolina at Chapel Hill\\Sumner, Kelsey Marie - nc_detect_one_drive\\Influenza Data\\cc_only\\180_days_WEEKLY\\weekly_shapefiles"
+  wd_cctriage = "C:\\Users\\joyceyan\\University of North Carolina at Chapel Hill\\Sumner, Kelsey Marie - nc_detect_one_drive\\Influenza Data\\ccandtriagenotes\\180_days_WEEKLY\\weekly_shapefiles"
   
 } else {
   print("Specify working directory")
@@ -42,7 +41,11 @@ setwd(wd_cconly)
 
 # look at a list of the directories in the current working directory
 dir_list = list.dirs()
-
+dir_list2 = lapply(dir_list, str_remove, pattern = ".") %>%
+  lapply(str_remove, pattern = "/") %>%
+  unlist()
+dir_list = dir_list2[which(nchar(dir_list2) > 0)]
+  
 # find latitude and longitude for the counties to use as basemap
 # get spatial data from tidy census
 nc_sf_counties = tidycensus::get_acs(geography = "county", state = "NC", 
@@ -55,30 +58,27 @@ nc_sf_counties = tidycensus::get_acs(geography = "county", state = "NC",
 dir.create("../weekly_plots")
 
 # now loop through every directory
-for (i in length(dir_list)){
+for (i in 1:length(dir_list)){
   
   # move into the next level directory
-  setwd(dir_list[i])
+  # setwd(dir_list[i])
   
   # load up area shape file:
-  cluster_area <- readOGR("sp_sig_clusters.shp")
+  cluster_area <- readOGR(file.path(getwd(), dir_list[i],"sp_sig_clusters.shp"))
   
   # open jpeg file
-  dir_name = str_remove(dir_list[i], ".")
-  jpeg(paste0("../../weekly_plots",dir_name,"_cluster_map.jpg"), width = 750, height = 750)
+  jpeg(paste0("../weekly_plots/",dir_list[i],"_cluster_map.jpg"), width = 750, height = 450)
   
   # now overlay the cluster area on the latitude and longitude map for counties
   plot(nc_sf_counties %>% st_geometry(),col="grey")
   plot(cluster_area, add=TRUE, col=alpha("red",0.4))
+  title(paste0("Influenza Space-Time Clusters for Week Ending ",dir_list[i]))
   
   # close the file and save the plot
   dev.off()
   
   
 }
-
-
-
 
 
 
---
+++ Kelsey/influenza_data/ncdetect_influenza_data_make_plots.R
@@ -0,0 +1,20 @@
+# ----------------------------------------- #
+#          NC DETECT Spatial Project        #
+#           Zip Code Influenza Data         #
+#     Make Plots of clusters Over Time      #
+#              August 1, 2019               #
+#                K. Sumner                  #
+# ----------------------------------------- #
+
+# what this is doing: 
+# reading in the influenza data and making plots of clusters over time with the week aggregation
+
+
+#### ------- load the libraries ---------- ####
+
+# load in tidyverse
+library(tidyverse)
+library(tidycensus)
+
+
+
